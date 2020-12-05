1d6f035fab7b4cf82f1da5448206f0a705be7875
Travis Mewborne <tmewborne@hotmail.com>
2020-12-04 19:04:23 -0500

    lab first room & slider puzzle info tab, worked on some stubborn doors

--- Assets/Scripts/ExitDoorBehavior.cs
+++ Assets/Scripts/ExitDoorBehavior.cs
@@ -4,27 +4,15 @@ using UnityEngine;
 
 public class ExitDoorBehavior : MonoBehaviour
 {
-    public float teleportX = 0;
-    public float teleportY = 0;
     public bool isLocked = true;
-    public Sprite unlockedDoorImage; //Only needs to be assigned if door is locked
+    public Sprite unlockedDoorImage;
 
     public void Update() {
-        //can move if player has cure here
         if (CureManager.Instance.playerHasCure) unlockDoor();
-        //Debug.Log("Usin the update on exitDoor");
     }
 
     public void unlockDoor() {
-        if(isLocked) this.GetComponent<SpriteRenderer>().sprite = unlockedDoorImage;
+        this.GetComponent<SpriteRenderer>().sprite = unlockedDoorImage;
         isLocked = false;
-        //Debug.Log("using the unlocked door");
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        /*
-        if("Player".Equals(collision.gameObject.tag) && CureManager.Instance.playerHasCure) {
-            //unlockDoor();
-        }*/
     }
 }
